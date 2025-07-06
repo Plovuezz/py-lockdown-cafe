b93811232e35463ad1e71e17af3506684c61ef3b
Vovas <vovaafon25@gmail.com>
2025-07-06 23:24:39 +0300

    Fixed smth

--- app/cafe.py
+++ app/cafe.py
@@ -14,9 +14,11 @@ class Cafe:
 
         today_date = datetime.date.today()
         vaccine_date = visitor["vaccine"]["expiration_date"]
-        if isinstance(vaccine_date, datetime.date):
-            if vaccine_date < today_date:
-                raise OutdatedVaccineError("Fix your vaccine")
+
+        if isinstance(vaccine_date, str):
+            vaccine_date = datetime.date.fromisoformat(vaccine_date)
+        if vaccine_date < today_date:
+            raise OutdatedVaccineError("Fix your vaccine")
 
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError("Wear your mask")
