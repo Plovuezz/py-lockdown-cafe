d8848af6e432ce9dc0d973591fb032fadffccc16
Vovas <vovaafon25@gmail.com>
2025-07-06 16:46:40 +0300

    Solution

---
+++ app/cafe.py
@@ -0,0 +1,24 @@
+import datetime
+from .errors import (NotVaccinatedError,
+                     OutdatedVaccineError,
+                     NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> None:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("Do vaccine")
+
+        today_date = datetime.date.today()
+        vaccine_date = visitor["vaccine"]["expiration_date"]
+
+        if vaccine_date < today_date:
+            raise OutdatedVaccineError("Fix your vaccine")
+
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError("Wear your mask")
+
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+from .cafe import Cafe
+from .errors import (NotWearingMaskError, VaccineError)
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+
+    not_vaccinated = 0
+    masks_to_buy = 0
+    friends_count = 0
+
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            not_vaccinated += 1
+        except NotWearingMaskError:
+            masks_to_buy += 1
+        else:
+            friends_count += 1
+
+    if not_vaccinated > 0:
+        return "All friends should be vaccinated"
+    elif masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    elif friends_count == len(friends):
+        return f"Friends can go to {cafe.name}"
